24f32d21475a9198ce6741e220f7c3825cbc8fae
O.D <olealea001@gmail.com>
2021-03-11 01:59:19 +0200

    Add files via upload

---
+++ GetStartedWithTestingAPI/src/test/java/Get200.java
@@ -0,0 +1,45 @@
+import entities.BaseClass;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+import static org.testng.AssertJUnit.assertEquals;
+
+public class Get200 extends BaseClass {
+
+
+    CloseableHttpClient client;
+    CloseableHttpResponse response;
+
+    @BeforeMethod
+    public void setup(){
+        client = HttpClientBuilder.create().build();
+    }
+
+    @AfterMethod
+    public void closeResources() throws IOException {
+        client.close();
+        response.close();
+    }
+
+    @Test
+    public void baseUrlReturns200() throws IOException {
+
+    }
+
+    @Test
+    public void rateLimitReturns200() throws IOException {
+
+    }
+
+    @Test
+    public void repositorySearchUrlReturns200() throws IOException {
+
+    }
+}
